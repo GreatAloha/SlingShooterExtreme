f8dfca2acda313326bd0bcc14500f1556138bed4
Marco J <Marco.juelich@stud.h-da.de>
2015-04-27 13:22:55 +0200

    LaunchPoint added.

---
+++ Assets/Scripts/SlingShot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingShot : MonoBehaviour {
+
+	private GameObject launchPoint;
+
+	void Awake(){
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+	}
+
+	void OnMouseEnter(){
+		//print ("SlingShot:MouseEnter");
+		launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit(){
+		//print ("SlingShot:MouseExit");
+		launchPoint.SetActive (false);
+	}
+
+}
