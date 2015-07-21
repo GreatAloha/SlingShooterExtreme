1752d9a330b0f8bcbfad737b9ced54ad115d851b
Marco J <Marco.juelich@stud.h-da.de>
2015-07-21 21:15:48 +0200

    Added destructible Worm.

---
+++ Assets/Scripts/WormDestruction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormDestruction : MonoBehaviour {
+		
+		public float Health = 150f;
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+		
+		void OnCollisionEnter(Collision col)
+		{
+			if (col.gameObject.GetComponent<Rigidbody>() == null) return;
+			
+			//if we are hit by a projectile
+			if (col.gameObject.tag == "Projectile")
+			{
+				GetComponent<AudioSource>().Play();
+				Destroy(gameObject);
+			}
+			else //we're hit by something else
+			{
+				//calculate the damage via the hit object velocity
+				float damage = col.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 10;
+				Health -= damage;
+				//don't play sound for small damage
+				if (damage >= 10)
+					GetComponent<AudioSource>().Play();
+				if (Health <= 0) Destroy(this.gameObject);
+			}
+		}
+	}
